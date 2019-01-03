95bc52cd7db7ea7301f14b938e047f3079b8fad8
Jefficus <jeff@smithicus.com>
2019-01-02 19:11:45 -0600

    Tweaked setup to check for vim, zsh, and tmux availability.

--- setupdotfiles.sh
+++ setupdotfiles.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
+
 src=$PWD/vimrc
 target=~/.vimrc
+if ! [ -x "$(vim --version)" ]; then
+    echo "Vim not installed locally."
+    echo "Run: sudo apt-get install vim"
+fi
 if [[ -e "$target" ]]; then
     echo "Moving old $target"
     mv $target ${target}-old
 fi
 echo "Linking new $target"
 ln -s $src $target
+
 
 src=$PWD/zshrc
 target=~/.zshrc
+if ! [ -x "$(zsh --version)" ]; then
+    echo "Zsh not installed locally."
+    echo "Run: sudo apt-get install zsh"
+fi
 if [[ -e "$target" ]]; then
     echo "Moving old $target"
     mv $target ${target}-old
@@ -17,8 +27,13 @@ fi
 echo "Linking new $target"
 ln -s $src $target
 
+
 src=$PWD/tmuxrc
 target=~/.tmux.conf
+if ! [ -x "$(tmux -V)" ]; then
+    echo "Tmux not installed locally."
+    echo "Run: sudo apt-get install tmux"
+fi
 if [[ -e "$target" ]]; then
     echo "Moving old $target"
     mv $target ${target}-old
